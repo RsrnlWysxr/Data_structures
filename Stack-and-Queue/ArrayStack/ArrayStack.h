8fd6be8d77beaa2ab5f714a993146a62ba94dc88
RsrnlWysxr <41769433+RsrnlWysxr@users.noreply.github.com>
2018-07-31 14:16:27 +0800

    Update ArrayStack.h

--- Stack-and-Queue/ArrayStack/ArrayStack.h
+++ Stack-and-Queue/ArrayStack/ArrayStack.h
@@ -207,21 +207,3 @@ class ArrayStack : public Stack<T>
 			}
 		}
 };
-
-int main()
-{
-	ArrayStack<int>* stack = new ArrayStack<int>();
-	for(int i = 0 ; i < 10 ; ++i) 
-	{
-		stack->push(i);
-	}
-	stack->toString();
-	cout << "top:" << stack->peek() <<endl;
-	for(int i = 0 ; i < 5 ; ++i)
-	{
-		stack->pop();
-	}
-	stack->toString();
-	cout << "top:" << stack->peek() <<endl;
-	return 0;
-}
